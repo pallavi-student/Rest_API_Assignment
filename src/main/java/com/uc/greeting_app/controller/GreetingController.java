b51cc9ebfba89b11715c08ea95c68f9ddd6f1537
pallavi15 <pallavigaharwar15@gmail.com>
2025-02-24 00:09:06 +0530

    [Pallavi] Add.uc1_GreetingApp

---
+++ src/main/java/com/uc/greeting_app/controller/GreetingController.java
@@ -0,0 +1,44 @@
+package com.uc.greeting_app.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/greeting")
+public class GreetingController {
+
+    @GetMapping
+    public Map<String, String> getGreeting() {
+        Map<String, String> response = new HashMap<>();
+        response.put("message", "Hello, this is a GET request!");
+        return response;
+    }
+
+    @PostMapping
+    public Map<String, String> postGreeting(@RequestBody Map<String, String> request) {
+        Map<String, String> response = new HashMap<>();
+        response.put("message", "Hello, this is a POST request!");
+        response.put("received", request.get("name"));
+        return response;
+    }
+
+    @PutMapping
+    public Map<String, String> putGreeting(@RequestBody Map<String, String> request) {
+        Map<String, String> response = new HashMap<>();
+        response.put("message", "Hello, this is a PUT request!");
+        response.put("updated", request.get("name"));
+        return response;
+    }
+
+    @DeleteMapping
+    public Map<String, String> deleteGreeting() {
+        Map<String, String> response = new HashMap<>();
+        response.put("message", "Hello, this is a DELETE request!");
+        return response;
+    }
+
+
+}
+
